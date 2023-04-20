81a41c40790c8488a9a6a5e138aecf1875e1d7f0
leandro-lcsistemas <lnascimntodesouza@gmail.com>
2023-04-19 22:52:54 -0300

    adicionando leitor de csv em tempo de execução - process

--- app/controllers/imadb_all_controllers.py
+++ app/controllers/imadb_all_controllers.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import io
 from app.models.Palettes import Palette
 from app.models.clinical_classification import ClinicalClassification
@@ -17,6 +19,7 @@ def search_cytokines_by_classification(classification: ClinicalClassification):
     data_all_informations_cytokines = get_cytokines_by_classification(classification)
     data_all_informations_cytokines = remover_chaves(data_all_informations_cytokines)
 
+    # csv em memória
     data_frame_cytokines_filter_by_classification = pd.json_normalize(data_all_informations_cytokines)
     csv_buffer = io.StringIO()
     data_frame_cytokines_filter_by_classification.to_csv(csv_buffer, index=False)
@@ -24,7 +27,14 @@ def search_cytokines_by_classification(classification: ClinicalClassification):
 
     csv_final = pd.read_csv(io.StringIO(csv_str))
 
-    return data_all_informations_cytokines
+    heatmap_graph = sns.heatmap(csv_final.corr(), annot=False, vmax=1, vmin=-1)
+
+    # Salva o plot em memória como um PNG em memória
+    png_buffer = io.BytesIO()
+    heatmap_graph.figure.savefig(png_buffer, format='png')
+    png_bytes = png_buffer.getvalue()
+    plt.clf()
+    return Response(content=png_bytes, media_type="image/png")
 
 
 @app.post("/heatmap")
